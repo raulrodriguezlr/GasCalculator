1117d951bc498cc272ef1b2cd2c42152f3e579ef
raulrodriguezlopez-rey <12rulo12@gmail.com>
2023-08-19 19:43:05 +0200

    Añadidido funcion de ventanas abiertas y aire puesto ademas de sus tooltip

--- app/src/main/java/com/example/gascalculator/MainActivity.kt
+++ app/src/main/java/com/example/gascalculator/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.gascalculator
 
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -28,8 +32,15 @@ class MainActivity : ComponentActivity() {
     private lateinit var precioGas: EditText
     private lateinit var consumo: EditText
     private lateinit var adView: AdView
+    private lateinit var ventanas:CheckBox
+    private lateinit var aire:CheckBox
+    private lateinit var infoWindow:ImageView
+    private lateinit var infoAir:ImageView
+    private lateinit var toolTipText:TextView
 
 
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,29 +51,57 @@ class MainActivity : ComponentActivity() {
         kmHechos = findViewById(R.id.editTextNumber)
         precioGas= findViewById(R.id.editTextNumberDecimal)
         consumo= findViewById(R.id.editTextNumberDecimal2)
-        MobileAds.initialize(this) {}
 
-        adView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        adView.loadAd(adRequest)
+        infoWindow = findViewById<ImageView>(R.id.infoWindow)
+        infoAir = findViewById<ImageView>(R.id.infoAir)
 
 
-        /*
-        // En tu función onCreate u otra función adecuada
-        buttonClick.setOnLongClickListener {
-            // Código a ejecutar cuando se realice una pulsación larga en el botón
-            textMessage.setTextColor(Pink40.toArgb())
-            true // Devuelve 'true' para indicar que el evento se ha consumido
-        }*/
+        val rootView = window.decorView.rootView as FrameLayout
+        val overlayView = layoutInflater.inflate(R.layout.tooltip, null)
+        toolTipText = overlayView.findViewById<TextView>(R.id.toolTipText)
+        windowInfo(rootView,overlayView)
+
+        //val inflater = LayoutInflater.from(this)
+        //val overlayView = inflater.inflate(R.layout.tooltip, rootView, false)
+
+
+
+
+        //inicializador de google ads
+            MobileAds.initialize(this) {}
+            adView = findViewById(R.id.adView)
+            val adRequest = AdRequest.Builder().build()
+            adView.loadAd(adRequest)
+
+        ventanas = findViewById<CheckBox>(R.id.ventanas)
+        aire = findViewById<CheckBox>(R.id.aire)
+
         val opciones = listOf("1 pasajero", "2 pasajeros", "3 pasajeros","4 pasajeros","5 pasajeros","6 pasajeros","7 pasajeros","8 pasajeros","9 pasajeros" ) // Agrega las opciones que desees
         showSpinner(opciones)
+        var multiplier:Float =1.0f
 
+        ventanas.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                multiplier = 1.06f
+            }
+            else{
+                multiplier = 1.0f
+
+            }
+        }
+        aire.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                multiplier = 1.1f
+            }
+            else{
+                multiplier = 1.0f
+
+            }
+        }
 
         buttonClick.setOnClickListener {
-            val resultado=recogerdatos()
+            val resultado= recogerdatos()?.times(multiplier)
             if (resultado!=null){
-
-                val resultadoFormateado = String.format("%.2f", resultado)
 
                 textMessage.text = "Cada persona tiene que pagar: %.2f €".format(resultado)
 
@@ -101,11 +140,75 @@ class MainActivity : ComponentActivity() {
 
 
     }
+    private fun windowInfo(rootView:ViewGroup,overlayView:View){
+        val layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        )
+        // Calcula la posición para centrar la superposición en el icono
+        //layoutParams.leftMargin = infoWindow.left + infoWindow.width / 2 - overlayView.width / 2
+        //layoutParams.topMargin = infoWindow.top + infoWindow.height / 2 - overlayView.height / 2
+
+        overlayView.translationX= (infoWindow.left + infoWindow.width / 2 - overlayView.width / 2).toFloat()
+        overlayView.translationY= (infoWindow.top + infoWindow.height / 2 - overlayView.height / 2).toFloat()
+
+
+
+        rootView.addView(overlayView)
+
+
+        // Configurar el OnClickListener para el icono de información
+        infoWindow.setOnClickListener {
+            toolTipText.text = "Ventana"
+            if (overlayView.parent == null) {
+                rootView.addView(overlayView)
+            }
+        }
+        // Configurar el OnClickListener para cerrar la superposición
+        overlayView.setOnClickListener {
+            rootView.removeView(overlayView)
+        }
+
+
+    }
+    private fun airInfo(rootView:ViewGroup,overlayView:View){
+        val layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+
+        )
+
+        // Calcula la posición para centrar la superposición en el icono
+        layoutParams.leftMargin = infoAir.left + infoAir.width / 2 - overlayView.width / 2
+        layoutParams.topMargin = infoAir.top + infoAir.height / 2 - overlayView.height / 2
+        overlayView.layoutParams = layoutParams
+        rootView.addView(overlayView)
+
+
+        // Configurar el OnClickListener para el icono de información
+        infoAir.setOnClickListener {
+            toolTipText.text = "Aire"
+
+            if (overlayView.parent == null) {
+                rootView.addView(overlayView)
+            }
+        }
+
+        // Configurar el OnClickListener para cerrar la superposición
+        overlayView.setOnClickListener {
+            rootView.removeView(overlayView)
+        }
+
+
+    }
+
+
+
     private fun calculo(km:Int,pasajeros:Int,precio:Float,consumo:Float):Float{
         val litrosPorKm=  (consumo * km)/100
         val precioTotal= litrosPorKm*precio
-        val precioPersona=precioTotal/pasajeros
-        return precioPersona
+
+        return precioTotal/pasajeros
     }
     private fun showAlertDialog(title: String, message: String) {
         val builder = AlertDialog.Builder(this)
@@ -158,21 +261,7 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
     }
 
-    /*
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContent {
-                AprendizajeTheme {
-                    // A surface container using the 'background' color from the theme
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        Greeting("Android")
-                    }
-                }
-            }
-        }*/
+
 }
 
 
